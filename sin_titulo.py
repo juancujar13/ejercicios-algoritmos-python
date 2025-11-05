76ded5758ca9433de667713cbfde174eb294db3f
juancujar13 <juancujar13@gmail.com>
2025-11-05 07:21:05 -0500

    Add files via upload

---
+++ sin_titulo.py
@@ -0,0 +1,6 @@
+print('ingrese el numero de horas trabajado')
+numerodehoras = float(input('Introduce numerodehoras: '))
+print('ingrese valor de la hora')
+valorhora = float(input('Introduce valorhora:'))
+salariopagar = numerodehoras*valorhora
+print("el salario a pagar es", salariopagar, "$")
